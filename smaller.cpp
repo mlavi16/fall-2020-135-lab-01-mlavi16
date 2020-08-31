552d8094faac6a67c084fe5e510004c33e28a600
mlavi16 <Maya.lavi11@gmail.com>
2020-08-31 11:07:07 -0400

    inital add of smaller.cpp

---
+++ smaller.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// using std::cout;
+// using std::cin;
+// using std::endl;
+
+int main()
+{
+	int first, second;
+
+	std::cout << "Enter the first number:";
+	std::cin >> first;
+	std::cout << "Enter the second number:";
+	std::cin >> second;
+	std::cout << "first is " << first << "\n";
+	std::cout << "second is " << second << "\n";
+
+	return 0;
+}
